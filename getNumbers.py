59a194cbf5e1271c62445eae3fb4780343068354
Danya Dye <Danya@users-MacBook-Pro.local>
2017-08-29 14:43:13 -0700

    Functioning reminder text using crontab.

--- getNumbers.py
+++ getNumbers.py
@@ -1,26 +1,42 @@
+#!/bin/bash
+
 from datetime import datetime
 import pytz
-from model import User, Objective, connect_to_db
+from model import User, Objective, connect_to_db, db, Message
 from server import app
-from flask import Flask
+import os
+
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+# Set number = to variable name
+twilio_number = os.environ['TWILIO_NUMBER']
 
 connect_to_db(app)
 
-with open('numberstotext.txt', 'a') as outFile:
+with open('numberstotext.txt', 'w') as outFile:
     # define what day's date is in YYYY-MM-DD format
     pacific = pytz.timezone('US/Pacific')
     today = datetime.now(tz=pacific).strftime('%Y-%m-%d')
-    print today
     # query db to get all objectives due today
     days_objectives = Objective.query.filter_by(due_date=today).all()
-#     # for each obj due, find user phone number
+    # for each obj due, find user phone number
     for each in days_objectives:
         obj = each.obj_id
-        print obj
+        # query db for user phone + set last_message_date
         user = User.query.filter_by(user_id=each.goal.user_id).first()
-        print user
+        user.last_message_date = today
         uphone = user.phone
-#         # write it to file
-        outFile.write('\n' + " " + str(uphone) + " " + str(obj) + " " + today)
+        # write it to file
+        outFile.write('\n' + str(uphone) + " " + str(obj) + " " + today)
+        db.session.commit()
 
-    # outFile.write('\n' + today)
+        # Twilio interaction
+        # send reminder that objective is due today
+        body = 'Text back "YES ' + str(obj) + '" if you completed this objective today.'
+        new_message = client.api.account.messages.create(to=uphone,
+                                                         from_=twilio_number,
+                                                         body=body)
--- scheduleCron.py
+++ scheduleCron.py
@@ -4,10 +4,10 @@ from crontab import CronTab
 my_cron = CronTab(user='vagrant')
 
 # where is that script located that needs to run?
-job = my_cron.new(command='env/bin/python && python /home/vagrant/src/AccountaBill/getNumbers.py')
+job = my_cron.new(command='python /home/vagrant/src/AccountaBill/getNumbers.py')
 
 # schedule this
-job.minute.every(1)
-# job.hour.every(1)
+# job.minute.every(1)
+job.hour.every(24)
 
 my_cron.write()
