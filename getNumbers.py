8b919b25bdca9e0043b5ea576e4b014ad87aa261
vagrant <vagrant@vagrant.vm>
2017-08-30 21:33:43 +0000

    Added source secrets.sh to crontab, fixing job.

--- getNumbers.py
+++ getNumbers.py
@@ -17,19 +17,27 @@ twilio_number = os.environ['TWILIO_NUMBER']
 
 connect_to_db(app)
 
-with open('numberstotext.txt', 'w') as outFile:
+with open('numberstotext.txt', 'a') as outFile:
     # define what day's date is in YYYY-MM-DD format
     pacific = pytz.timezone('US/Pacific')
+    print pacific
+    print '****'
     today = datetime.now(tz=pacific).strftime('%Y-%m-%d')
+    print today
+    print '****'
     # query db to get all objectives due today
     days_objectives = Objective.query.filter_by(due_date=today).all()
     # for each obj due, find user phone number
     for each in days_objectives:
         obj = each.obj_id
+        print obj
+        print '****'
         # query db for user phone + set last_message_date
         user = User.query.filter_by(user_id=each.goal.user_id).first()
         user.last_message_date = today
+        print user.last_message_date
         uphone = user.phone
+        print uphone
         # write it to file
         outFile.write('\n' + str(uphone) + " " + str(obj) + " " + today)
         db.session.commit()
